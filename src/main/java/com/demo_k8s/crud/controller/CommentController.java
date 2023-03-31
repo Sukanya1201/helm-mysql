f5d9baa6c3a62d41f6e986fe03de43b0221ae099
Sayanta Roy <bobby.1823@gmail.com>
2023-03-31 13:42:03 +0530

    second commit for mysql helm

--- src/main/java/com/demo_k8s/crud/controller/CommentController.java
+++ src/main/java/com/demo_k8s/crud/controller/CommentController.java
@@ -1,7 +1,7 @@
 package com.demo_k8s.crud.controller;
 
-import com.demo_k8s.crud.entity.Comment;
-import com.demo_k8s.crud.service.CommentService;
+//import com.demo_k8s.crud.entity.Comment;
+//import com.demo_k8s.crud.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,21 +11,26 @@ import java.util.List;
 @RequestMapping("/comments")
 public class CommentController {
 
-    @Autowired
-    private CommentService service;
-
-    @PostMapping
-    public Comment addComments(@RequestBody Comment comment){
-        return service.addComment(comment);
-    }
+//    @Autowired
+//    private CommentService service;
+//
+//    @PostMapping
+//    public Comment addComments(@RequestBody Comment comment){
+//        return service.addComment(comment);
+//    }
+//
+//    @GetMapping
+//    public List<Comment> getComments(){
+//        return service.getComments();
+//    }
 
     @GetMapping
-    public List<Comment> getComments(){
-        return service.getComments();
+    public String hello(){
+        return "OK";
     }
 
-    @GetMapping("/{id}")
-    public Comment getCommentsById(@PathVariable int id){
-        return service.getCommentById(id);
-    }
+//    @GetMapping("/{id}")
+//    public Comment getCommentsById(@PathVariable int id){
+//        return service.getCommentById(id);
+//    }
 }
